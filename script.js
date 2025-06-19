efa7bb138ec635bf9cbe598488af17c43eb15a3f
Lakshmi Raj <lakshmiraj9847506759@gmail.com>
2025-06-19 09:02:18 +0530

    Added logic for creating a new noe.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const note=document .getElementById("note")
 const saveBtn=document.getElementById("save-btn")
 const allNotes=document.getElementById("all-notes")
+
+function newNote(){
+    const inputNote=note.value.trim()
+    if(!inputNote){
+alert("Please enter text!")
+return
+    }
+    
+    const noteElt=document.createElement("div")
+    noteElt.classList.add("note")
+    noteElt.textContent=inputNote
+    allNotes.appendChild(noteElt)
+    note.value=""
+}
+saveBtn.addEventListener("click", newNote)
