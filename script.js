4f378b60e06ece7c119105ec8553469f09a8b796
Lakshmi Raj <lakshmiraj9847506759@gmail.com>
2025-06-19 09:44:41 +0530

    Added sorting by timestamp and improved note rendering

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const note=document .getElementById("note")
 const saveBtn=document.getElementById("save-btn")
 const allNotes=document.getElementById("all-notes")
 
-function renderNote(text){
+function renderNote(noteObj){
     const noteElt=document.createElement("div")
     noteElt.classList.add("note")
-    noteElt.textContent=text
+    const date = new Date(noteObj.time).toLocaleString(); // for readable time
+    noteElt.innerHTML = `<strong>${date}</strong><br>${noteObj.text}`
     allNotes.appendChild(noteElt)
 }
 function loadNotes(){
     const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    notes.sort((a, b)=>b.time - a.time)
 notes.forEach(renderNote)
 }
 //Function to add a new note 
@@ -19,12 +21,18 @@ function newNote(){
 alert("Please enter text!")
 return
     }
-    renderNote(inputNote)
-    note.textContent=""
+    const newNoteObj={
+    text: inputNote,
+    time: Date.now()
+}
+    renderNote(newNoteObj)
+    note.value=""
 
 //saving data to localStorage
 let notes = JSON.parse(localStorage.getItem("notes")) || [];
-notes.push(inputNote)
+
+
+notes.push(newNoteObj)
 localStorage.setItem("notes", JSON.stringify(notes));
 
 
